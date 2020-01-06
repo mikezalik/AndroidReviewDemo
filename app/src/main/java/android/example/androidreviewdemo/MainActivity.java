663b94e049bd444bac1a8aaba82499f883dfc7fa
mikezalik <mike.zalik@gmail.com>
2020-01-06 12:02:17 -0800

    Layout and consolidate onclick/password function

--- app/src/main/java/android/example/androidreviewdemo/MainActivity.java
+++ app/src/main/java/android/example/androidreviewdemo/MainActivity.java
@@ -11,15 +11,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view)   {
         EditText nameEditText = findViewById(R.id.nameEditText);
-
-        Log.i("info", "You've pressed the button!");
-        Log.i("Values", nameEditText.getText().toString());
-    }
-
-    public void passwordFunction (View view) {
         EditText passwordEditText = findViewById (R.id.passwordEditText);
 
         Log.i ("password", passwordEditText.getText().toString());
+        Log.i("info", "You've pressed the button!");
+        Log.i("Values", nameEditText.getText().toString());
     }
 
     @Override
