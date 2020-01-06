348310839877f8cd4c10b60abd99ae33211177e8
mikezalik <mike.zalik@gmail.com>
2020-01-06 11:37:17 -0800

    nameEditText

--- app/src/main/java/android/example/androidreviewdemo/MainActivity.java
+++ app/src/main/java/android/example/androidreviewdemo/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickFunction (View view)   {
+    public void clickFunction(View view)   {
+
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+
         Log.i("info", "You've pressed the button!");
+
+        Log.i("Values", nameEditText.getText().toString());
     }
 
     @Override
