7f7053f2fd71304614cc7bde6d196b78155ef5d6
mikezalik <mike.zalik@gmail.com>
2020-01-06 12:21:03 -0800

    Layout constraint refactor and update

--- app/src/main/java/android/example/androidreviewdemo/MainActivity.java
+++ app/src/main/java/android/example/androidreviewdemo/MainActivity.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void clickFunction(View view)   {
+    public void clickFunction(View view) {
         EditText nameEditText = findViewById(R.id.nameEditText);
         EditText passwordEditText = findViewById (R.id.passwordEditText);
 
         Log.i ("password", passwordEditText.getText().toString());
         Log.i("info", "You've pressed the button!");
         Log.i("Values", nameEditText.getText().toString());
+
+        Toast.makeText(this, "Welcome!", Toast.LENGTH_LONG).show();
     }
 
     @Override
