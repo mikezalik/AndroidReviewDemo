215d9e45721e2b482731e12fb1f65b39d53d4d1f
mikezalik <mike.zalik@gmail.com>
2020-01-06 11:58:06 -0800

    Fixed warnings for redundancy in editText

--- app/src/main/java/android/example/androidreviewdemo/MainActivity.java
+++ app/src/main/java/android/example/androidreviewdemo/MainActivity.java
@@ -10,14 +10,14 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view)   {
-        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        EditText nameEditText = findViewById(R.id.nameEditText);
 
         Log.i("info", "You've pressed the button!");
         Log.i("Values", nameEditText.getText().toString());
     }
 
     public void passwordFunction (View view) {
-        EditText passwordEditText = (EditText) findViewById (R.id.passwordEditText);
+        EditText passwordEditText = findViewById (R.id.passwordEditText);
 
         Log.i ("password", passwordEditText.getText().toString());
     }
